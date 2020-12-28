a5daa49176c2140467ce2e45d78e50cb10277bb4
Jeremy Orellana <jorellanam89@gmail.com>
2020-12-28 18:20:05 -0500

    Final project of the day completed

---
+++ Day_1/final_project.py
@@ -0,0 +1,19 @@
+#1. Create a greeting for your program.
+
+print("Hi! welcome to the best alias generator")
+
+#2. Ask the user for the city that they grew up in.
+
+city = input("First, enter the city were you grew up in\n")
+
+#3. Ask the user for the name of a pet.
+
+pet_name = input("And, what is the name of your pet?\n")
+
+#4. Combine the name of their city and pet and show them their band name.
+
+alias = "The alias that best describes you is: " + pet_name + " " + city
+
+print(alias)
+
+#5. Make sure the input cursor shows on a new line,
