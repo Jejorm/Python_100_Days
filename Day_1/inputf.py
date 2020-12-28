d42e96a75a67e0f4df0e630233defda86509a978
Jeremy Orellana <jorellanam89@gmail.com>
2020-12-28 17:03:13 -0500

    Input file and exercise added

---
+++ Day_1/inputf.py
@@ -0,0 +1,4 @@
+# input() will get user input in console 
+# Then print() will print the word "Hello" and the user input
+# ctrl + / will comment or uncomment a line automatically
+print("Hello " + input("What is your name? ") + "!")
