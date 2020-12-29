a7dd495e23723bbc24c9a85556e0a89d2e5ec376
Jeremy Orellana <jorellanam89@gmail.com>
2020-12-29 16:33:33 -0500

    First data types exercise

---
+++ Day_2/data_types.py
@@ -0,0 +1,23 @@
+#Data Types
+
+#String
+
+print("Hello"[0])
+print("Hello"[4])
+print("Hello" + "world")
+print("123" + "456")
+
+#Integer
+
+print(123 + 456)
+
+123_456_789
+
+#Float
+
+3.14159
+
+#Boolean
+
+True
+False
