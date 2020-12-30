c9021d38c1d77f02bf1575b77ce9cf5cdf63ac0c
Jeremy Orellana <jorellanam89@gmail.com>
2020-12-30 14:00:01 -0500

    Operations exercises comleted

---
+++ Day_2/operations.py
@@ -0,0 +1,18 @@
+3 + 5
+7 - 4
+3 * 2
+6 / 3
+2 ** 3
+
+
+# PEMDASLR
+
+# Parentheses  ()
+# Exponents  **
+# Multiplication  *
+# Division /
+# Addition  +
+# Substraction  -
+
+
+print(3 * (3 + 3) / 3 - 3)
