35c2acf3292eb2f15f809b61e03b1c49b513b1a7
Jeremy Orellana <jorellanam89@gmail.com>
2020-12-30 15:00:33 -0500

    Your life in weeks challenge completed

---
+++ Day_2/your_life_in_weeks.py
@@ -0,0 +1,10 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age?")
+# 🚨 Don't change the code above 👆
+
+years = 90 - int(age)
+days = years * 365
+weeks = years * 52
+months = years * 12
+
+print(f"You have {days} days, {weeks} weeks, and {months} months left.")
