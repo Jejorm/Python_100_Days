9967a8253e60818102328cf5841c51fc362711cd
Jeremy Orellana <jorellanam89@gmail.com>
2020-12-31 14:43:05 -0500

    Index error exercise

--- Day_4/lists.py
+++ Day_4/lists.py
@@ -1,11 +1,19 @@
 states_of_america = ["Delaware", "Pencilvania", "New Jersey", "Georgia", "Connecticut", "Massachusetts"]
 
-print(states_of_america[3])
-print(states_of_america[-1])
+# print(states_of_america[3])
+# print(states_of_america[-1])
 
-states_of_america[1] = "Pennsylvania"
+# states_of_america[1] = "Pennsylvania"
 
-states_of_america.append("Maryland")
+# states_of_america.append("Maryland")
 
-states_of_america.extend(["South Carolina", "New Hampshire", "Virginia"])
-print(states_of_america)
+# states_of_america.extend(["South Carolina", "New Hampshire", "Virginia"])
+
+num_of_states = len(states_of_america)
+print(states_of_america[5])
+print(states_of_america[num_of_states-1])
+
+# Index Error
+
+# print(states_of_america[6])
+print(states_of_america[num_of_states])
