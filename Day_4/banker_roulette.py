a2bb6fcbf33701a935738d9dd589240cdcd5ccc3
Jeremy Orellana <jorellanam89@gmail.com>
2020-12-31 14:01:41 -0500

    Banker roulette challenge completed

---
+++ Day_4/banker_roulette.py
@@ -0,0 +1,13 @@
+import random
+
+names_string = input("Give me everybody's names, separated by a comma. ")
+names = names_string.split(", ")
+
+
+len_names = len(names)
+
+random_number = random.randint(0, len_names-1)
+
+random_name = names[random_number]
+
+print(f"{random_name} is going to buy the meal today!")
