93df48b5d4b01a7fe7149856db790719b4bd587d
Jeremy Orellana <jorellanam89@gmail.com>
2020-12-31 13:37:45 -0500

    Lists exercises

---
+++ Day_4/lists.py
@@ -0,0 +1,11 @@
+states_of_america = ["Delaware", "Pencilvania", "New Jersey", "Georgia", "Connecticut", "Massachusetts"]
+
+print(states_of_america[3])
+print(states_of_america[-1])
+
+states_of_america[1] = "Pennsylvania"
+
+states_of_america.append("Maryland")
+
+states_of_america.extend(["South Carolina", "New Hampshire", "Virginia"])
+print(states_of_america)
