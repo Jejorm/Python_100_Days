7f0bc60e3d40c120808e3612c5cd2e35c20cf957
Jeremy Orellana <jorellanam89@gmail.com>
2021-01-06 13:55:31 -0500

    Final project of the day completed (auction program)

---
+++ Day_9/auction_program/main.py
@@ -0,0 +1,25 @@
+from art import logo
+
+print(logo)
+
+continue_auction = True
+person = {}
+highest_bid = 0
+
+while continue_auction:
+    name = input("What's your name? ")
+    bid = int(input("What's you bid? "))
+
+    person[name] = bid
+
+    answer = input("Are there any other bridders? Tpe 'yes' or 'no'.\n").lower()
+    if answer == "no":
+        continue_auction = False
+
+for name_person in person:
+    actual_bid = person[name_person]
+    if actual_bid > highest_bid:
+        highest_bid = actual_bid
+        winner = name_person
+
+print(f"The winner is {winner.title()} with a bid of ${highest_bid}")
